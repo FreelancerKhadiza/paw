e923eeff38d431a5d5b8423fe899bc9814ac0655
Khadiza Khatun <freelancerkhadiza2@gmail.com>
2024-12-23 07:50:47 +0600

    env change

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,12 +1,24 @@
 import mongoose from "mongoose";
-import "dotenv/config"
-import app from "./app";
+import app, { bot } from "./app";
+import "dotenv/config";
 
 async function main() {
-    await mongoose.connect(process.env.MONGODB_ATLAS_URI as string);
-    app.listen(process.env.PORT, () =>{
-        console.log(`http://localhost:${process.env.PORT}/`);
-    })
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    const port = process.env.PORT || 3000;
+    
+    app.listen(port, async () => {
+        console.log(`Server is running on port ${port}`);
+        const webhookUrl = `https://${process.env.SERVER_URL}/api/bot`;
+        try {
+            // await bot?.launch(); 
+
+            await bot.telegram.setWebhook(webhookUrl);// for production
+            console.log(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=https://${process.env.SERVER_URL}/api/bot`);
+            
+        } catch (err) {
+            console.error("Error setting webhook:", err);
+        }
+    });
 
 }
 
